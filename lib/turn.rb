8a6fc4f88ffc9df48fdc0e28fb99c3961acd007f
Jessica dos Santos <jndossantos00@gmail.com>
2020-11-23 21:09:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -35,10 +35,8 @@ def turn(board)
    user_input = gets.strip
    index = input_to_index(user_input)
    if !valid_move?(board, index)
-     position_taken?(board, index)
-     
-    turn(board)
-
+     move(board,index,player)
+     turn(board)
+   end
     display_board(board)
-end
 end
