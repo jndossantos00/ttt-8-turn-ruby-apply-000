2deb08778d4c1168c05d09c715f463c7d1a38585
Jessica dos Santos <jndossantos00@gmail.com>
2020-11-23 21:06:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,7 @@ def turn(board)
    index = input_to_index(user_input)
    if !valid_move?(board, index)
      position_taken?(board, index)
-     move(board, index)
+     
     turn(board)
 
     display_board(board)
